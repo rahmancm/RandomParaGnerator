ff7297ce51453d4c51840eeb22433b716e1fd172
rahmancm <arahmancm@gmail.com>
2020-12-13 02:24:44 +0530

    generate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import data from "./data";
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  const submitHandle = (e) => {
+    e.preventDefault();
+    let number = parseInt(count);
+    if (count <= 0) {
+      number = 1;
+    }
+    if (count > 9) {
+      number = 9;
+      alert("You can have maximum 9 paragraphs");
+    }
+
+    setText(data.slice(0, number));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum ..? </h3>
+      <h4>Lets get something else </h4>
+      <form onSubmit={submitHandle}>
+        <label htmlFor="number">How many Paragraphs :</label>
+        <input
+          name="number"
+          type="number"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
   );
 }
 
